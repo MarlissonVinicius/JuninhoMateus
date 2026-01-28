4ed4a550aa7d7875165508540bb6ef1968442af9
Mateus Cruz <mateuscolvr@gmail.com>
2026-01-28 19:37:22 -0300

    formate o código direito, seu peste

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 type Token =
   | { tipo: 'numero', valor: number }
   | { tipo: 'mais' }
-type Ast = {
-  tipo: "soma", x: number, y: number
-}
+
+type Ast = { tipo: 'soma', x: number, y: number }
+
 class Lexer {
   code: string;
   idx: number = 0;
@@ -14,7 +14,6 @@ class Lexer {
   }
 
   isNumber(char: string): boolean {
-
     if (char === '0') { return true; }
     if (char === '1') { return true; }
     if (char === '2') { return true; }
@@ -46,24 +45,24 @@ class Lexer {
           this.next();
 
           nextChar = this.peek();
-          if (nextChar === null) {
-            break;
-          }
+          if (nextChar === null) break;
         }
 
         this.tokens.push({ tipo: 'numero', valor: parseInt(number) });
-        this.next()
-      } else if (code[this.idx] === "+") {
-        this.tokens.push({ tipo: "mais" })
+        this.next();
+      } else if (code[this.idx] === '+') {
+        this.tokens.push({ tipo: 'mais' });
         this.next();
       }
     }
+
     return this.tokens;
   }
 
   next(): string | null {
     if (this.idx + 1 < this.code.length) {
       this.idx += 1;
+
       return this.code[this.idx];
     }
 
@@ -71,9 +70,7 @@ class Lexer {
   }
 
   peek(): string | null {
-    if (this.idx + 1 < this.code.length) {
-      return this.code[this.idx + 1];
-    }
+    if (this.idx + 1 < this.code.length) return this.code[this.idx + 1];
 
     return null;
   }
@@ -88,11 +85,12 @@ class Parser {
   }
 
   parse(): Ast[] {
-    const tokenAtual = this.tokens[this.idx]
-    const token2 = this.tokens[this.idx + 1]
-    const token3 = this.tokens[this.idx + 2]
-    if (tokenAtual.tipo === "numero" && token2.tipo === "mais" && token3.tipo == "numero") {
-      this.ast.push({ x: tokenAtual.valor, tipo: "soma", y: token3.valor })
+    const tokenAtual = this.tokens[this.idx];
+    const token2 = this.tokens[this.idx + 1];
+    const token3 = this.tokens[this.idx + 2];
+
+    if (tokenAtual.tipo === 'numero' && token2.tipo === 'mais' && token3.tipo == 'numero') {
+      this.ast.push({ x: tokenAtual.valor, tipo: 'soma', y: token3.valor });
     }
 
     return this.ast;
@@ -108,16 +106,16 @@ class Interpreter {
   }
 
   interp(): number {
-    const x: number = this.ast[0].x
-    const y: number = this.ast[0].y
+    const x: number = this.ast[0].x;
+    const y: number = this.ast[0].y;
 
     return x + y;
   }
 }
 
-const code = "14+3"
+const code = '14+2'
 const lexer = new Lexer(code);
 const parser = new Parser(lexer.lex())
-const interpreter = new Interpreter(parser.parse())
+const interpreter = new Interpreter(parser.parse());
 
 console.log(interpreter.interp());
