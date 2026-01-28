43892bdc43339d4deb735322e54f4a5fbb4bbc98
marlisson <marlissonvinicius13@gmail.com>
2026-01-28 17:57:11 -0300

    adicionado funcionalidade de analisar léxicamente o simbolo + em tipo mais

--- main.ts
+++ main.ts
@@ -1,19 +1,18 @@
-type Token =
+type Token = //Criação do tipo token, onde se for do tip número terá um valor que será um número 
   | { tipo: 'numero', valor: number }
   | { tipo: 'mais' }
 
-const code = '1222'
-
-class Lexer {
-  code: string;
-  idx: number = 0;
-  tokens: Token[] = [];
+class Lexer { 
+  code: string; //atributo code, que será utilizado internamente na classe 
+  idx: number = 0; //ponteiro que dirá qual posiçao atual está sendo lida 
+  tokens: Token[] = []; //array do tipo token que receberá todos os textos que foram convertidos 
 
   constructor(code: string) {
     this.code = code;
   }
 
-  isNumber(char: string): boolean {
+  isNumber(char: string): boolean { //método que verifica se argumento passado é um número 
+    //caso seja, retornará verdadeiro 
     if (char === '0') { return true; }
     if (char === '1') { return true; }
     if (char === '2') { return true; }
@@ -27,49 +26,57 @@ class Lexer {
 
     return false;
   }
+  
+  lex(): Token[] { //método que fará a conversão de string para token 
+    while (this.idx < code.length) { 
+      console.log(this.idx, code.length,code[this.idx], code[this.idx+1])
+      if (this.isNumber(code[this.idx])) { //verifica se o caracterer atual que estou é um número 
+        let number = code[this.idx]; //pega o número atual e guardar 
+        let nextChar = this.peek(); //recebe a posição do próximo índice  
 
-  lex(): Token[] {
-    if (this.isNumber(code[this.idx])) {
-      let number = code[this.idx];
-      let nextChar = this.peek();
-
-      if (nextChar === null) {
-        throw new Error('Invalid code');
-      }
-
-      while (this.isNumber(nextChar)) {
-        number = number + nextChar;
-        this.next();
-
-        nextChar = this.peek();
-        if (nextChar === null) {
-          break;
+        if (nextChar === null) { //verificando se o próximo caractere é nulo 
+          //throw new Error('Invalid code'); //caso seja, lanço uma exceção. 
+          break; 
         }
+
+        while (this.isNumber(nextChar)) { //enquanto o próximo caracter for um número 
+          number = number + nextChar; //concatenando o valor atual com o próximo 
+          this.next(); //avançando o ponteiro 
+
+          nextChar = this.peek(); //observo o próximo número 
+          if (nextChar === null) {
+            break;
+          }
+        }
+
+        this.tokens.push({ tipo: 'numero', valor: parseInt(number) }); // converto a string em inteiro e armazeno o tipo e valor no array de tokens 
+        this.next()
+      } else if (code[this.idx] === "+") {
+        this.tokens.push({ tipo: "mais" })
+        this.next(); 
       }
-
-      this.tokens.push({ tipo: 'numero', valor: parseInt(number) });
     }
-
     return this.tokens;
   }
 
   next(): string | null {
-    if (this.idx + 1 < this.code.length) {
-      this.idx += 1;
-      return this.code[this.idx];
+    if (this.idx + 1 < this.code.length) { //verificando se a próxima casa existe 
+      this.idx += 1; //se existir, eu passo para ela 
+      return this.code[this.idx]; //me retorna o caracter da próxima casa 
     }
 
-    return null;
+    return null; //caso não tenha próxima casa, me retorna nulo 
   }
 
-  peek(): string | null {
-    if (this.idx + 1 < this.code.length) {
-      return this.code[this.idx + 1];
+  peek(): string | null { //método espiada 
+    if (this.idx + 1 < this.code.length) { //se a próximo indice for menor que o tamanho máximo da minha string 
+      return this.code[this.idx + 1]; //retornar a próxima posição 
     }
 
     return null;
   }
 }
 
+const code = '+1222+1345' //Constante que receberá o comando 
 const lexer = new Lexer(code);
 console.log(lexer.lex());
